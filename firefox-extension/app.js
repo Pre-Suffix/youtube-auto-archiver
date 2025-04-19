9f12254315929f1e35dd0f05f8f8202fb3210056
Pre-Suffix <contato@presuffix.net>
2025-04-19 11:04:47 -0300

    0.1.2

--- firefox-extension/app.js
+++ firefox-extension/app.js
@@ -3,15 +3,15 @@ const PASS = "password";
 const URL = "http://localhost:8080/?pass=" + PASS + "&id=";
 
 // Function to request video save
-async function saveVideo(tab) {
+async function saveVideo(tab, full = false) {
   const url = new URL(tab.url);
   // If the URL doesn't correspond to a valid youtube video, exit function
   if(url.hostname != "www.youtube.com" || url.pathname != "/watch" || !url.searchParams.has("v")) return false;
 
   // Fetch ID from URL, then send the proper API call
   let id = url.searchParams.get("v");
-  await fetch(URL + id);
-
+  await fetch(URL + id + "&full=" + full ? "yes" : "no");
+  
   return true;
 }
 
@@ -23,7 +23,16 @@ browser.contextMenus.create({
   contexts: ["tab"]
 });
 
+// Setup context menu
+const saveVideoFullID = "save-yt-video-full"
+browser.contextMenus.create({
+  id: saveVideoFullID,
+  title: "Save this video w/ extras",
+  contexts: ["tab"]
+});
+
+
 // eslint-disable-next-line no-unused-vars
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  if(info.menuItemId == saveVideoID) saveVideo(tab);
+  if(info.menuItemId == saveVideoID || info.menuItemId == saveVideoFullID) saveVideo(tab, info.menuItemId == saveVideoFullID);
 });
